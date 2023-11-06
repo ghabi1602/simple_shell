797fe9f92fff5dd69e554365ec96a7250df4fcdc
Abdessattar <ghabi.1602@gmail.com>
2023-11-06 21:25:12 +0000

    Add a new function

--- functions.c
+++ functions.c
@@ -39,3 +39,41 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }
+/**
+ * _realloc - reallocates a new memory space
+ * @ptr: pointer to an allocated memory space
+ * @old_size: integer
+ * @new_size: integer
+ * Return: pointer to the newly allocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *p1;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+	p1 = ptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = p1[i];
+	}
+	if (new_size < old_size)
+		for (i = 0; i < new_size; i++)
+			p[i] = p1[i];
+	free(ptr);
+	return (p);
+}

71bcce481c5fc18b8dbc1b838e4fcbf52ed9bfe9
Abdessattar <ghabi.1602@gmail.com>
2023-11-07 18:34:25 +0000

    Add a prototype

--- shell.h
+++ shell.h
@@ -20,5 +20,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif /* SHELL_H */

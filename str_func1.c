67f96be252742f7d73c619088dffbe085f049678
Abdessattar <ghabi.1602@gmail.com>
2023-11-08 18:06:55 +0000

    Add new functions

---
+++ str_func1.c
@@ -0,0 +1,120 @@
+#include "shell.h"
+
+/**
+ * _memset - fills memory with a constant type
+ * @s: string
+ * @b: char
+ * @n: integer
+ * Return: pointer to a string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		s[i] = b;
+		n--;
+		i++;
+	}
+	return (s);
+}
+
+/**
+ * _memcpy - copies memory area
+ * @dest: string
+ * @src: string
+ * @n: unsigned integer
+ * Return: pointer to dest string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (n > 0)
+	{
+		dest[i] = src[i];
+		n--;
+		i++;
+	}
+	return (dest);
+}
+/**
+ * _realloc - reallocates a new memory space
+ * @ptr: pointer to an allocated memory space
+ * @old_size: integer
+ * @new_size: integer
+ * Return: pointer to the newly allocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p, *p1;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+	p1 = ptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = p1[i];
+	}
+	if (new_size < old_size)
+		for (i = 0; i < new_size; i++)
+			p[i] = p1[i];
+	free(ptr);
+	return (p);
+}
+/**
+ * _strcat - concatinates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1, len2;
+
+	len1 = 0;
+	while (dest[len1] != '\0')
+		len1++;
+
+	len2 = 0;
+	while (src[len2] != '\0')
+	{
+		dest[len1] = src[len2];
+		len2++;
+		len1++;
+	}
+	return (dest);
+}
+/**
+ * _strcpy - copies a string
+ * @dest: string to copy in
+ * @src: string to be copied
+ * Return: pointer to the new copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0, i;
+
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
---
+++ str_func2.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to be duplicated
+ * Return: pointer to the new string
+ */
+char *_strdup(const char *str)
+{
+	char *dupstr;
+	int len = 0, j;
+
+	if (!str)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	dupstr = malloc(sizeof(char) * len + 1);
+
+	if (!dupstr)
+
+		return (NULL);
+
+	for (j = 0; j < len; j++)
+	{
+		dupstr[j] = str[j];
+	}
+	return (dupstr);
+}
+
+/**
+ * _strcspn - calculates the length of the initial segment of s
+ * which consists entirely of bytes not in reject.
+ * @str: the string to check within
+ * @reject: the set of characters to reject
+ * Return: length of segment
+ */
+size_t _strcspn(const char *str, const char *reject)
+{
+	size_t i, j, len;
+
+	if (!str || !reject)
+		return (0);
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	if (reject[0] == '\0')
+		return (len);
+
+	i = 0;
+	j = 0;
+	while (i < len)
+	{
+		j = 0;
+		while (reject[j] != '\0')
+		{
+			if (str[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcmp - compares two strings
+ * @s: string
+ * @str: string
+ * Return: -1 1 or 0(equal)
+ */
+int _strcmp(const char *s, const char *str)
+{
+	int len1, len2, i;
+
+	len1 = _strlen(s);
+	len2 = _strlen(str);
+
+	if (len1 != len2)
+		return (-1);
+
+	for (i = 0; i < len1; i++)
+	{
+		if (s[i] != str[i])
+			return (-1);
+	}
+	return (1);
+}

b38a2d59328e3f25043c5cd98afc950730baed7c
Abdessattar <ghabi.1602@gmail.com>
2023-11-12 21:48:15 +0000

    Delete

--- built_ins.c
+++ built_ins.c
@@ -58,3 +58,47 @@ int _setenv(char **exp)
 		i++; }
 	return (1);
 }
+/**
+ * _unsetenv - deletes an environment
+ * @exp: array of input strings
+ * Return: int
+ */
+int _unsetenv(char **exp)
+{
+	int len = 0, i = 0, j;
+	char *p, *tok;
+
+	while (exp[len] != NULL)
+		len++;
+	if (len != 2)
+	{
+		perror("Failed: number of arguments");
+		return (-1);
+	}
+	p = _getenv(exp[1]);
+	if (!p)
+	{
+		perror("Failed: variable doesnt exist!");
+		return (-1);
+	}
+	while (environ[i] != NULL)
+	{
+		tok = strtok(environ[i], '=');
+		if (_strcmp(tok, exp[1]) == 1)
+		{
+			for (j = i; environ[j] != NULL; j++)
+			{
+				environ[j] = _realloc(environ[j], sizeof(char)
+* _strlen(environ[j]), sizeof(char) * _strlen(environ[j + 1]));
+
+				if (!environ[j])
+					return (-1);
+				environ[j] = _strdup(environ[j + 1]);
+			}
+			free(environ[j]);
+			return (1);
+		}
+		i++;
+	}
+	return (-1);
+}
